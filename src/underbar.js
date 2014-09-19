47875a38c2f55a31a8aecc01b146856a36f02933
Christopher Treviño <christophertrev@gmail.com>
2014-09-18 20:03:25 -0700

    implimented Last: Successful

--- src/underbar.js
+++ src/underbar.js
@@ -39,6 +39,9 @@ var _ = {};
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    var index = array.length -n;
+    ( index <0 ) ? index = 0 : index = index;  
+    return n === undefined ? array[array.length-1] : array.slice(index, array.length)
   };
 
   // Call iterator(value, key, collection) for each element of collection.
